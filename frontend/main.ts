2a2b1fa4544a87981892bd5c601cec2683cdd3bb
KeyToLove <coderEDC@163.com>
2023-04-27 09:31:28 +0800

    feat: enable Vercel Web Analytics

--- frontend/main.ts
+++ frontend/main.ts
@@ -4,6 +4,10 @@ import App from './App.vue'
 import 'highlight.js/styles/shades-of-purple.css'
 import naive from 'naive-ui'
 
+// Vercel Web Analytics
+import { inject } from '@vercel/analytics'
+inject()
+
 const app = createApp(App)
 app.use(naive)
 app.mount('#app')
